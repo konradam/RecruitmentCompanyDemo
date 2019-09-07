41ba11a1d2300d737281a3be6ed3cf733572d316
Konrad <Konrad@DESKTOP-9UOHKHM>
2019-09-07 16:50:29 +0200

    Adding attributes to JobCandidate model

--- RecruitmentCompanyDemo/Models/JobCandidate.cs
+++ RecruitmentCompanyDemo/Models/JobCandidate.cs
@@ -1,12 +1,23 @@
-﻿namespace RecruitmentCompanyDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecruitmentCompanyDemo.Models
 {
     public class JobCandidate
     {
         public int Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
         public string JobTitle { get; set; }
+
+        [Required]
         public string ComapnyName { get; set; }
+
         public bool IsApplicationAccepted { get; set; }
     }
 }

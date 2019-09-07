f9f2c80ef3a92e4dbbc6e659651d2f334b919ae6
Konrad <Konrad@DESKTOP-9UOHKHM>
2019-09-07 12:59:21 +0200

    Adding a new model JobCandidate

---
+++ RecruitmentCompanyDemo/Models/JobCandidate.cs
@@ -0,0 +1,12 @@
+﻿namespace RecruitmentCompanyDemo.Models
+{
+    public class JobCandidate
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string ComapnyName { get; set; }
+        public bool IsApplicationAccepted { get; set; }
+    }
+}
